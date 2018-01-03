77a8bda2e393398b76e5817730d66dea9667d7f7
chelsea596949 <qa860605@gmail.com>
2018-01-03 12:01:21 +0800

    小改 版面跑掉

--- finalpj/login.php
+++ finalpj/login.php
@@ -1,3 +1,9 @@
+<?php
+session_start();
+//require("dbconnect.php");
+//set the login mark to empty
+$_SESSION['uid'] = 0;
+?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
