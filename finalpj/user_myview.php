0dcca5a6688fc22ef10282d112c8a5a12cbd7af7
s104213049 <s104213049@mail1.ncnu.edu.tw>
2018-01-04 19:10:34 +0800

    些微修改

--- finalpj/user_myview.php
+++ finalpj/user_myview.php
@@ -26,7 +26,7 @@
 <button class="tool" onclick="location.href='user_upload.php'">Upload</button><br />
 <button class="tool" onclick="location.href='user_myview.php'">Myview</button>
 </div>
-<a href='homepage.php'><img src="image/logout.png" id="logout"/></a>
+<a href='homepage.php' title ='登出'><img src="image/logout.png" id="logout"/></a>
 </div>
 <div id="content">
 <div id="search">
